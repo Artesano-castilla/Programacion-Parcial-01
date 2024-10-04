3ffdb4a185879650f37ebe59a4e9f0aa2ab65107
Joel-Amendolare <joel.amendolare@davinci.edu.ar>
2024-10-04 19:45:19 -0300

    codigo

--- js/parcial.js
+++ js/parcial.js
@@ -10,28 +10,47 @@
 let discos = [];
 // Función que solicita los datos del disco al usuario y lo guarda:
 const cargarNuevoDisco = () => {
-    let posDisco;
-    let disco = {
-      nombre: prompt(`Ingrese el nombre del disco`) ?? "",
-      autor: prompt(`Ingrese el autor del disco`) ?? "",
-      codigo: solicitarCodigo,//agregar y validar codigo unico //!joel
-      pistas: solicitarPistas //!richard
-    };
-    //agregar ordenado
-    discos.push(disco);
-    //
+  let posDisco;
+  let disco = {
+    nombre: prompt(`Ingrese el nombre del disco`) ?? "",
+    autor: prompt(`Ingrese el autor del disco`) ?? "",
+    codigo: solicitarCodigo()//agregar y validar codigo unico //!joel
+    
+  }
 
-  console.table(discos);
-  //console.table(discos[0].pistas);
-
+ pistas: solicitarPistas //!richard
 };
+//agregar ordenado
+discos.push(disco);
+//
 
-const solicitarCodigo = () => {//!joel
+console.table(discos);
+//console.table(discos[0].pistas);
+
+;
+
+function solicitarCodigo() {//!joel
   //agregamos codigo del disco
   //posDisco= discos[i]
-  return codigoIngresado
 
+  let codigoValido = false;
+  let codigoIngresado;
+
+  while (!codigoValido) {
+    codigoIngresado = parseInt(prompt("Ingrese el código numérico del disco (entre 1 y 999)"));
+    if (isNaN(codigoIngresado)) {
+      alert("Ingrese un número válido.");
+    } else if (codigoIngresado < 1 || codigoIngresado > 999) {
+      alert("El código debe estar entre 1 y 999.");
+    } else if (discos.find(disco => disco.codigo === codigoIngresado)) {
+      alert("El código ya existe. Ingrese otro.");
+    } else {
+      codigoValido = true;
+    }
+  }
+  return codigoIngresado; //! No se si estará bien richard?
 }
+
 
 
 const solicitarPistas = () => {//!Richard
@@ -39,15 +58,15 @@ const solicitarPistas = () => {//!Richard
   do {
     let pista = {
       nombre: prompt(`Ingrese el nombre de la pista`) ?? "",
-      duracion: prompt(`Ingrese el duracion de la pista`) ?? "",        
+      duracion: prompt(`Ingrese el duracion de la pista`) ?? "",
     };
     //validacion pista max
   } while (condition);//confirm
-pi
-  
-  
-//Se crea un arreglo con un objeto pista con nombre  y duracion
-//pistaMax:-infinity ,
+  pi
+
+
+  //Se crea un arreglo con un objeto pista con nombre  y duracion
+  //pistaMax:-infinity ,
 
 }
 // Función que recorre todos los discos y los muestra en la etiqueta #respuesta:

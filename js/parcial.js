bd2a17c0543ca2abfeb7a28eeb541d5902d4e929
ricardo <ricardo.garcia@gba.gob.ar>
2024-10-03 21:34:12 -0300

    estructura cargar disco

--- js/parcial.js
+++ js/parcial.js
@@ -6,26 +6,58 @@
 
 //+prompt(`Ingrese el código numérico del disco`) ?? ''
 // Array que almacenará los discos agregados por el usuario:
-let discos = [];
 
+let discos = [];
 // Función que solicita los datos del disco al usuario y lo guarda:
 const cargarNuevoDisco = () => {
- // do {
+    let posDisco;
     let disco = {
       nombre: prompt(`Ingrese el nombre del disco`) ?? "",
       autor: prompt(`Ingrese el autor del disco`) ?? "",
-      // codigo: agregarCodigo,//agregar y validar codigo unico
-      // pistas: agregarPistas //Se crea un arreglo con un objeto pista con nombre  y duracion
+      codigo: solicitarCodigo,//agregar y validar codigo unico //!joel
+      pistas: solicitarPistas //!richard
     };
+    //agregar ordenado
     discos.push(disco);
-  //} while (condition);//desea ingresar otro disco???confirm
+    //
 
-  // console.table(discos.disco[0]);
+  console.table(discos);
+  //console.table(discos[0].pistas);
+
 };
 
+const solicitarCodigo = () => {//!joel
+  //agregamos codigo del disco
+  //posDisco= discos[i]
+  return codigoIngresado
+
+}
+
+
+const solicitarPistas = () => {//!Richard
+  let pistas = [];
+ 
+  do {
+    let pista = {
+      nombre: prompt(`Ingrese el nombre de la pista`) ?? "",
+      duracion: prompt(`Ingrese el duracion de la pista`) ?? "",        
+    };
+    //validacion pista max
+  } while (condition);//confirm
+pi
+  
+  
+//Se crea un arreglo con un objeto pista con nombre  y duracion
+//pistaMax:-infinity ,
+
+}
 // Función que recorre todos los discos y los muestra en la etiqueta #respuesta:
 const mostrarDiscos = () => {
+  //cantidad de pistas = pistas.length -1
   // Variable que almacenará el contenido a mostrar dentro de #respuesta:
+  let duracionTotal;
+  let promedioDuracion;
+  let pistaMax = -Infinity;
   let respuesta = "";
 
   // Ejemplo de cómo generar contenido:
